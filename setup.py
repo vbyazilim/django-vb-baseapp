8b9cc688fb167c8a23bc354ec5a0aaa69e480934
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2019-10-20 09:30:30 +0300

    Fix PyPI classifier

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.2.4',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
